5f3394b2c58c7eb3d6aa1875d3b9af1d61ee682e
Théo <theopohin86@gmail.com>
2024-04-08 17:00:00 +0200

    first commit

---
+++ src/main/java/fr/eni/lebonfoin/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+    package fr.eni.lebonfoin.config;
+    import javax.sql.DataSource;
+
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.provisioning.JdbcUserDetailsManager;
+    import org.springframework.security.provisioning.UserDetailsManager;
+    import org.springframework.security.web.SecurityFilterChain;
+
+    @Configuration
+    @EnableWebSecurity
+    @EnableMethodSecurity
+    public class SecurityConfig {
+
+
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeHttpRequests((authorize) -> authorize
+                            .requestMatchers("/css/**", "/favicon.ico", "/", "/index").permitAll()
+                            .requestMatchers("/user").hasAnyRole("USER")
+                            .requestMatchers("/admin").hasAnyRole("ADMIN")
+                            .anyRequest().authenticated()
+                    )
+                    .formLogin(login -> login
+                            .defaultSuccessUrl("/home")
+                            .permitAll())
+                    .logout(logout -> logout
+                            .logoutSuccessUrl("/"));
+            return http.build();
+        }
+
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        UserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+            JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+            jdbcUserDetailsManager.setUsersByUsernameQuery("select nom, mot_de_passe, 1 from users where nom=?");
+            jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select nom, administrateur from users  where nom=?");
+            return jdbcUserDetailsManager;
+        }
+    }
---
+++ src/main/java/fr/eni/lebonfoin/controller/HomeController.java
@@ -0,0 +1,24 @@
+package fr.eni.lebonfoin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/accueil")
+    public String user() {
+        return "Hello, User!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello, Admin!";
+    }
+
+}
