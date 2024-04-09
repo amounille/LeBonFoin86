0e6fdbdbfd19099b6185316e69338c8af3ae55ed
rbouyaghi2023 <rickos2003@gmail.com>
2024-04-09 14:04:18 +0200

    MAJ

--- src/main/java/fr/eni/lebonfoin/controller/HomeController.java
+++ src/main/java/fr/eni/lebonfoin/controller/HomeController.java
@@ -1,24 +1,18 @@
 package fr.eni.lebonfoin.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping
-    public String home() {
-        return "Hello, World!";
+    public String index() {
+        return "index";
     }
 
     @GetMapping("/home")
-    public String user() {
+    public String home() {
         return "home";
     }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "Hello, Admin!";
-    }
-
 }
