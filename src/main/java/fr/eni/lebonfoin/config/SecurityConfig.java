2494a5bd6753a55b0ec3248a1e70dc3487409ce4
rbouyaghi2023 <rickos2003@gmail.com>
2024-04-10 10:03:49 +0200

    MAJ ENI

--- src/main/java/fr/eni/lebonfoin/config/SecurityConfig.java
+++ src/main/java/fr/eni/lebonfoin/config/SecurityConfig.java
@@ -1,13 +1,12 @@
 package fr.eni.lebonfoin.config;
-import javax.sql.DataSource;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -18,21 +17,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/css/**", "/favicon.ico", "/", "/index", "/home", "/registration","/error").permitAll()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/css/**", "/favicon.ico", "/", "/index", "/home", "/registration", "/error", "/reset-password").permitAll()
                         .requestMatchers("/user").hasAnyRole("USER")
                         .requestMatchers("/admin").hasAnyRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
                 .formLogin(login -> login
-                        .defaultSuccessUrl("/home")
+                        .loginPage("/login")
+                        .loginProcessingUrl("/perform_login")
+                        .defaultSuccessUrl("/home", true)
                         .permitAll())
                 .logout(logout -> logout
-                        .logoutSuccessUrl("/login"));
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll());
         return http.build();
     }
 
@@ -45,7 +45,7 @@ public class SecurityConfig {
     UserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
         jdbcUserDetailsManager.setUsersByUsernameQuery("select nom, mot_de_passe, 1 from UTILISATEURS where nom=?");
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select nom, administrateur from UTILISATEURS  where nom=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select nom, administrateur from UTILISATEURS where nom=?");
         return jdbcUserDetailsManager;
     }
 }
