26326f49f1e5fb8684383fa5a99099b01de962f0
Théo <theopohin86@gmail.com>
2024-04-11 09:24:41 +0200

    delete article

--- src/main/java/fr/eni/lebonfoin/controller/ArticleController.java
+++ src/main/java/fr/eni/lebonfoin/controller/ArticleController.java
@@ -13,12 +13,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -34,7 +30,6 @@ public class ArticleController {
     private final ArticleService articleService;
     private final UserService userService;
 
-
     @Autowired
     public ArticleController(ArticleService articleService, UserService userService) {
         this.articleService = articleService;
@@ -42,34 +37,30 @@ public class ArticleController {
     }
 
     @GetMapping("/new/article")
-    public String getArticeNew(Model model) {
+    public String getArticleNew(Model model) {
         model.addAttribute("article", new Article());
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = auth.getName();
         User user = userRepository.findByPseudo(username);
         Long userId = user.getNoUtilisateur();
-//        System.out.println(userId);
 
         List<Categorie> categories = categorieRepository.findAll();
 
         model.addAttribute("categories", categories);
-        model.addAttribute("userId",userId);
+        model.addAttribute("userId", userId);
         model.addAttribute("username", username);
         return "articleNew";
     }
 
     @PostMapping("/new/article")
-    public String saveArticle(@RequestParam("categoryId") String categorieId, Article article, Model model) {
-        System.out.println("cocucouuoccocuocoucocuo");
+    public String saveArticle(@RequestParam("categoryId") String categoryId, Article article, Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = auth.getName();
         User user = userRepository.findByPseudo(username);
         Long userId = user.getNoUtilisateur();
         article.setNoUtilisateur(userId);
-        Categorie categorie = categorieRepository.findById(Long.valueOf(categorieId)).orElseThrow(() -> new IllegalArgumentException("Catégorie non trouvée"));
+        Categorie categorie = categorieRepository.findById(Long.valueOf(categoryId)).orElseThrow(() -> new IllegalArgumentException("Catégorie non trouvée"));
         article.setNoCategorie(categorie.getNo_categorie());
-
-
 
         articleRepository.save(article);
 
@@ -78,11 +69,23 @@ public class ArticleController {
         return "articleNew";
     }
 
+    @PostMapping(value = "/article/{noArticle}", params = "_method=delete")
+    public String deleteArticleHiddenMethod(@PathVariable("noArticle") Long articleNo) {
+        return deleteArticle(articleNo);
+    }
+
+    @DeleteMapping("/article/{noArticle}")
+    public String deleteArticle(@PathVariable("noArticle") Long articleNo) {
+        Article articleToDelete = articleRepository.findById(articleNo)
+                .orElseThrow(() -> new IllegalArgumentException("Article non trouvé avec le numéro : " + articleNo));
+        articleRepository.delete(articleToDelete);
+        return "redirect:/articles";
+    }
+
     @GetMapping("/articles")
-    public String getAllUsers(Model model) {
+    public String getAllArticles(Model model) {
         List<Article> articles = articleService.getAllArticles();
         model.addAttribute("articles", articles);
         return "articles";
     }
-
 }
