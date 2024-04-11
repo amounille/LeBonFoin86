1e0309351ae67bd5b3aca4f28c6a8e05f64c4248
Théo <theopohin86@gmail.com>
2024-04-11 09:51:39 +0200

    crud article

--- src/main/java/fr/eni/lebonfoin/controller/ArticleController.java
+++ src/main/java/fr/eni/lebonfoin/controller/ArticleController.java
@@ -82,6 +82,47 @@ public class ArticleController {
         return "redirect:/articles";
     }
 
+    @GetMapping("/edit/article/{noArticle}")
+    public String editArticle(@PathVariable("noArticle") Long articleNo, Model model) {
+        Article article = articleRepository.findById(articleNo)
+                .orElseThrow(() -> new IllegalArgumentException("Article non trouvé avec le numéro : " + articleNo));
+        List<Categorie> categories = categorieRepository.findAll();
+
+        model.addAttribute("article", article);
+        model.addAttribute("categories", categories);
+        return "articleEdit";
+    }
+
+    @PostMapping("/edit/article/{noArticle}")
+    public String updateArticle(@PathVariable("noArticle") Long articleNo, @RequestParam("categoryId") String categoryId, Article updatedArticle) {
+        Article articleToUpdate = articleRepository.findById(articleNo)
+                .orElseThrow(() -> new IllegalArgumentException("Article non trouvé avec le numéro : " + articleNo));
+        Long originalUserId = articleToUpdate.getNoUtilisateur();
+        Double categoryIdAsDouble = Double.valueOf(categoryId.replace(",", "."));
+        Long categoryIdAsLong = categoryIdAsDouble.longValue();
+        Categorie categorie = categorieRepository.findById(categoryIdAsLong)
+                .orElseThrow(() -> new IllegalArgumentException("Catégorie non trouvée"));
+
+        articleToUpdate.setNomArticle(updatedArticle.getNomArticle());
+        articleToUpdate.setDescription(updatedArticle.getDescription());
+        articleToUpdate.setDateDebutEncheres(updatedArticle.getDateDebutEncheres());
+        articleToUpdate.setDateFinEncheres(updatedArticle.getDateFinEncheres());
+        articleToUpdate.setPrixInitial(updatedArticle.getPrixInitial());
+        articleToUpdate.setPrixVente(updatedArticle.getPrixVente());
+
+        // Réutiliser l'utilisateur d'origine pour l'article mis à jour
+        articleToUpdate.setNoUtilisateur(originalUserId);
+        articleToUpdate.setNoCategorie(categorie.getNo_categorie());
+
+        // Enregistrer les modifications dans la base de données
+        articleRepository.save(articleToUpdate);
+
+        return "redirect:/articles";
+    }
+
+
+
+
     @GetMapping("/articles")
     public String getAllArticles(Model model) {
         List<Article> articles = articleService.getAllArticles();
